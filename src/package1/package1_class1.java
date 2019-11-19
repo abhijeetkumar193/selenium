21d60b0781456bc4486b0afe3faccf4b599c6ca3
abhijeetkumar193@gmail.com <abhijeetkumar193@gmail.com>
2019-11-19 22:11:15 +0530

    File committed

---
+++ src/package1/package1_class1.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class package1_class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main method");
+
+	}
+
+}
---
+++ src/package1/package1_class2.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class package1_class2 {
+
+	private void syso() {
+		// TODO Auto-generated method stub
+		System.out.println("inmcide class2 under package1");
+	}
+}
